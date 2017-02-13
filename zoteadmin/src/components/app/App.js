5b910b53ee35ea03fc879fe339e9244258ea81c9
Madison Kerndt <madison.kerndt@gmail.com>
2017-02-12 18:47:41 -0700

    route to different components

--- zoteadmin/src/components/app/App.js
+++ zoteadmin/src/components/app/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-// import './App.css';
+import { Link, browserHistory } from 'react-router';
 import './App.scss';
+
+import Foo from '../foo/Foo';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <nav>
+            Links:
+            {' '}
+            <Link to="/">Home</Link>
+            {' '}
+            <Link to="/foo">Foo</Link>
+          {/* <div>
+            <button onClick={() => browserHistory.push('/foo')}>Go to /foo</button>
+          </div> */}
+        </nav>
+         {this.props.children}
       </div>
     );
   }
--- zoteadmin/src/index.js
+++ zoteadmin/src/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
+
+import reducers from './reducers/index';
+import App from './components/app/App';
+import Foo from './components/foo/Foo';
+
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    routing: routerReducer
+  })
+);
+
+const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <Route path="foo" component={Foo}/>
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('root')
-);
+)
