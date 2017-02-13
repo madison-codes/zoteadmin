b1e89ec065c03f5777b2fa8a72eed0f63e4b0f83
Madison Kerndt <madison.kerndt@gmail.com>
2017-02-12 21:15:49 -0700

    redux store set up

--- zoteadmin/src/components/app/App.js
+++ zoteadmin/src/components/app/App.js
@@ -9,29 +9,8 @@ import Groups from '../groups/Groups';
 
 import { search } from '../../actions/search'
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <h2>Welcome to React</h2>
-//         </div>
-//         <input type='text'
-//           placeholder='search'
-//           onKeyUp={(e) => search(e.target.value)}>
-//         </input>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <Nav />
-//          {this.props.children}
-//       </div>
-//     );
-//   }
-// }
 
-
-function App({ search }) {
+function App({ search, children }) {
   return (
     <div className="App">
       <div className="App-header">
@@ -41,12 +20,12 @@ function App({ search }) {
         placeholder='search'
         onKeyUp={(e) => search(e.target.value)}>
       </input>
-      {/* {this.props.} */}
       <p className="App-intro">
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
       <Nav />
-       {/* {this.props.children} */}
+      { children }
+       {/* {routing} */}
     </div>
   )
 }
