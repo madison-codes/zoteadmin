2b8fa1830be39f7f00f839a372c07e3f76f9b59f
Madison Kerndt <madison.kerndt@gmail.com>
2017-02-26 23:02:22 -0700

    select user functionality added

--- src/components/selectedUser/SelectedUser.js
+++ src/components/selectedUser/SelectedUser.js
@@ -4,16 +4,24 @@ import { Link } from 'react-router';
 
 class SelectedUser extends Component {
   render(){
+    const user = this.props.state.selectUser;
     return(
       <div>
-        <button>
+        <form onSubmit={() => console.log('update user')}>
+          <input type='text' defaultValue={user.username}></input>
+          <input type='text' defaultValue={user.sub_end}></input>
+          <input type='text' defaultValue={user.stripe_id}></input>
           <Link
             onClick={() => this.props.selectUser({})}
-            to="/users"> Exit
+            to="/users"><button type='submit'>Submit Changes</button>
           </Link>
+        </form>
+        <button>
+        <Link
+        onClick={() => this.props.selectUser({})}
+        to="/users"> Cancel
+        </Link>
         </button>
-
-        <h1>Working</h1>
       </div>
     )
   }
